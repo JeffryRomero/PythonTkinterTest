3870725d6b5cb5b4c1fd9a69c80d2021a12b6f0a
Jeffry Romero <jeffryromero@unitec.edu>
2019-11-08 09:21:11 -0600

    Prueba de tkinter

---
+++ tkinterprueba.py
@@ -0,0 +1,41 @@
+from tkinter import *
+from tkinter.ttk import *
+import sys
+
+class Window():
+    def __init__(self):
+        self.window = Tk()
+        self.lbl = None
+        
+    def title(self, title):
+        if isinstance(title, str):
+            self.window.title(title)
+        else:
+            print('ingrese un titulo adecuado')
+    
+    def label(self, labeltxt):
+        if isinstance(labeltxt, str):
+            self.lbl = Label(self.window, text=labeltxt, style="BW.TLabel")
+            self.lbl.grid(column=0, row=0)
+        else:
+            print('ingrese un adecuado adecuado')
+
+    def styleForDarkcula(self):
+        self.style = Style()
+        self.style.configure("BW.TLabel", foreground="black", background="white")
+
+        """ 
+        Example:
+            l1 = ttk.Label(text="Test", style="BW.TLabel")
+            l2 = ttk.Label(text="Test", style="BW.TLabel")
+        """
+
+    def run(self):
+        self.window.mainloop()
+
+if __name__ == "__main__":
+    myapp = Window()
+    myapp.title(sys.argv[1])
+    myapp.styleForDarkcula()
+    myapp.label(sys.argv[2])
+    myapp.run()
